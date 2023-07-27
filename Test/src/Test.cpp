cd77d98e32aa0cbe1e9ac0540966b935b96d1946
Batishavo <romero.martinez.carlosraul@gmail.com>
2023-07-27 09:00:55 -0600

    first commit

---
+++ Test/src/Test.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	if (__cplusplus == 201703L)
+			cout << "C++17\n";
+		else if (__cplusplus == 201402L)
+			cout << "C++14\n";
+		else if (__cplusplus == 201103L)
+			cout << "C++11\n";
+		else if (__cplusplus == 199711L)
+			cout << "C++98\n";
+		else
+			cout << "pre-standard C++\n";
+	return 0;
+}
